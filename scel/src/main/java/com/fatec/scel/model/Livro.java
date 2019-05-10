e7e6081a7ad82054d8458dd8bcc89f3ad0403c5c
Laboratorio101 <Laboratorio101@LAB101-15>
2019-05-10 15:57:05 -0300

    Inclusão da classe usuário

--- scel/src/main/java/com/fatec/scel/model/Livro.java
+++ scel/src/main/java/com/fatec/scel/model/Livro.java
@@ -1,16 +1,27 @@
 package com.fatec.scel.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity(name = "Livro")
 public class Livro {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotEmpty(message = "O isbn deve ser preenchido")
 	private String isbn;
+	@NotEmpty(message = "O titulo deve ser preenchido")
 	private String titulo;
+	@NotNull(message = "Autor invalido")
+	@Size(min = 1, max = 10, message = "Autor deve ter entre 1 e 10 caracteres")
 	private String autor;
 
-	public Livro() {
+	protected Livro() {
 	}
 
 	public Livro(String i, String t, String a) {
@@ -24,9 +35,6 @@ public class Livro {
 	}
 
 	public void setIsbn(String isbn) {
-		if (isbn == "" | isbn == null) {
-			throw new RuntimeException("ISBN invalido");
-		}
 		this.isbn = isbn;
 	}
 
@@ -35,7 +43,6 @@ public class Livro {
 	}
 
 	public void setTitulo(String titulo) {
-
 		this.titulo = titulo;
 	}
 
--- scel/src/test/java/com/fatec/scel/UC01CadastrarLivro.java
+++ scel/src/test/java/com/fatec/scel/UC01CadastrarLivro.java
@@ -1,74 +1,72 @@
 package com.fatec.scel;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import javax.validation.ConstraintViolationException;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.fatec.scel.model.Livro;
+import com.fatec.scel.model.LivroRepository;
 
+@RunWith(SpringRunner.class)
+@DataJpaTest
 public class UC01CadastrarLivro {
-	@Test
-	public void CT01CadastrarLivroComDadosValidos() {
-		try {
-			// cenario
-			Livro umLivro = new Livro();
-			// acao
-			umLivro.setIsbn("121212");
-			umLivro.setTitulo("Engenharia de Softwar");
-			umLivro.setAutor("Pressman");
-		} catch (RuntimeException e) {
-			// verificacao
-			fail("nao deve falhar");
-		}
+	@Autowired
+	private LivroRepository livroRepository;
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
+	private Livro livro;
+
+	@Before
+	public void start() {
+		livro = new Livro("1111", "Engenharia de Software", "Pressman");
 	}
 
 	@Test
-	public void CT02cadastrarLivroComISBN_em_branco() {
-		// cenario
-		Livro livro = new Livro();
-
-		livro.setTitulo("Engenharia de Software");
-		livro.setAutor("Pressman");
-		try {
-			// acao
-			livro.setIsbn("");
-			fail("deveria lançar uma exceção");
-		} catch (RuntimeException e) {
-			// verificacao
-			assertEquals("ISBN invalido", e.getMessage());
-		}
+	public void ct01_saveComISBN_NuloDeveLancarException() throws Exception {
+		expectedException.expect(ConstraintViolationException.class);
+		expectedException.expectMessage("O isbn deve ser preenchido");
+		livro.setIsbn(null);
+		livroRepository.save(livro);
 	}
-	@Test
-	public void CT03cadastrarLivroComISBN_nulo() {
-		// cenario
-		Livro livro = new Livro();
 
-		livro.setTitulo("Engenharia de Software");
-		livro.setAutor("Pressman");
-		try {
-			// acao
-			livro.setIsbn(null);
-			fail("deveria lançar uma exceção");
-		} catch (RuntimeException e) {
-			// verificacao
-			assertEquals("ISBN invalido", e.getMessage());
-		}
-	}
 	@Test
-	public void CT04cadastrarLivroCom_obtem_ISBN() {
-		// cenario
-		Livro umLivro = new Livro();
-		try {
-			// acao
-			umLivro.setIsbn("121212");
-			umLivro.setTitulo("Engenharia de Softwar");
-			umLivro.setAutor("Pressman");
-		} catch (RuntimeException e) {
-			// verificacao
-			fail("nao deve falhar");
-		}
-		assertEquals("121212", umLivro.getIsbn());
-		
+	public void ct02_saveComTituloNuloDeveLancarException() throws Exception {
+		expectedException.expect(ConstraintViolationException.class);
+		expectedException.expectMessage("O titulo deve ser preenchido");
+		livro.setTitulo(null);
+		livroRepository.save(livro);
+	}
+
+	@Test
+	public void ct03_saveComTituloBrancoDeveLancarException() throws Exception {
+		expectedException.expect(ConstraintViolationException.class);
+		expectedException.expectMessage("O titulo deve ser preenchido");
+		livro.setTitulo("");
+		livroRepository.save(livro);
+	}
+
+	@Test
+	public void ct04_saveComAutorInvalidoSizeDeveLancarException() throws Exception {
+		expectedException.expect(ConstraintViolationException.class);
+		expectedException.expectMessage("Autor deve ter entre 1 e 10 caracteres");
+		livro.setAutor("engenharia dddddd dddddddddddddd");
+		livroRepository.save(livro);
+	}
+	
+
+	@Test
+	public void ct05_saveComAutorInvalidoNuloDeveLancarException() throws Exception {
+		expectedException.expect(ConstraintViolationException.class);
+		expectedException.expectMessage("Autor invalido");
+		livro.setAutor(null);
+		livroRepository.save(livro);
 	}
 }
+
